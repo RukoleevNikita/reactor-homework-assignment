d8c8d7019984e32bb21c00f442d512618d306d43
RukoleevNikita <rukoleev.nikita@yandex.ru>
2021-03-29 14:54:39 +0600

    onBlur Class

--- src/Counter/CounterClass.js
+++ src/Counter/CounterClass.js
@@ -4,11 +4,17 @@ import PropTypes from 'prop-types'
 import cls from './Counter.module.css'
 
 import { Button } from 'semantic-ui-react'
-// import { Input } from 'semantic-ui-react'
+import { Input } from 'semantic-ui-react'
 
 export default class Some extends React.Component {
+    static propTypes = {
+        min: PropTypes.number.isRequired,  
+        max: PropTypes.number.isRequired   
+     }
+
     state = {
-        val: this.props.min
+        val: this.props.min,
+        inputValue: this.props.min
     }
 
     increase = () => {
@@ -21,14 +27,41 @@ export default class Some extends React.Component {
 
     set(newVal) {
         let val = Math.min(Math.max(newVal, this.props.min), this.props.max)
-        this.setState({val : val})
+        this.setState({
+            val : val,
+            inputValue: val 
+        }) 
     }
+
+    setValue(newStr) {
+        this.setState({inputValue: newStr})
+        // let val = parseInt(newStr)
+
+        // if(isNaN(val)) {
+        //     val = this.props.min
+        // }
+        // this.set(isNaN(val) ? this.props.min : val)
+    }
+
+    applyValue = () => {
+        let val = parseInt(this.state.inputValue)
+        this.set(isNaN(val) ? this.props.min : val)
+    }
+
     render() {
         return (
             <div className={cls.items}>
+                {this.state.val} <br/>
+                {this.state.inputValue}
                 <h1>{this.state.val}</h1>
                 <div className={cls.item}>
                     <Button size='mini' onClick={this.decrease}>-</Button>
+                    <Input 
+                        placeholder='...' 
+                        onChange={(e) => this.setValue(e.target.value)} 
+                        value={this.state.inputValue}
+                        onBlur={this.applyValue}
+                    /> 
                     <Button size='mini' onClick={this.increase}>+</Button>
                 </div>
             </div>
@@ -41,7 +74,3 @@ export default class Some extends React.Component {
 //     max: 5 
 // }
 
-Some.propTypes = {
-  min: PropTypes.number.isRequired,  
-  max: PropTypes.number.isRequired   
-}
